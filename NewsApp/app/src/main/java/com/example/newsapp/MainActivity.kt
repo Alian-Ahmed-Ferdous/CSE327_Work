17fe87ac045d19de1850e8e7f3a5d6a95c3e1d18
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-10-01 23:41:10 +0600

    initial design of a news reading app to learn RecyclerView

---
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity(), NewsItemClicked{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = NewsListAdapter()
+        recyclerView.adapter = adapter
+
+
+    }
+
+    override fun onItemClicked(item: String) {
+        
+    }
+}
