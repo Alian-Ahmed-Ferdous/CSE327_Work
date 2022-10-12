ba7484bd48e0c360d1f831ed48f4df55bbea170d
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-10-12 21:14:53 +0600

    express tutorial

--- Node Tutorial/Express/app.js
+++ Node Tutorial/Express/app.js
@@ -149,3 +149,88 @@
 // app.listen(5000, () => {
 //   console.log('Server is listening on port 5000....')
 // })
+
+// >> Middlewire << 
+
+// const express = require('express')
+// const app = express()
+// const morgan = require('morgan')
+// const logger = require('./logger')
+// const authorize = require('./authorize')
+// //  req => middleware => res
+
+// // app.use([logger, authorize])
+// // app.use(express.static('./public'))
+// app.use(morgan('tiny'))
+
+// app.get('/', (req, res) => {
+//   res.send('Home')
+// })
+// app.get('/about', (req, res) => {
+//   res.send('About')
+// })
+// app.get('/api/products', (req, res) => {
+//   res.send('Products')
+// })
+// app.get('/api/items', (req, res) => {
+//   console.log(req.user)
+//   res.send('Items')
+// })
+
+// app.listen(5000, () => {
+//   console.log('Server is listening on port 5000....')
+// })
+
+
+//>>Methods<<
+
+
+const express = require('express')
+const app = express()
+let { people } = require('./data')
+
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
+})
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).json({ success: true, person: name })
+})
+
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).json({ success: true, data: [...people, name] })
+})
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
+})
+
+
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
