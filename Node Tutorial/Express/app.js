9b44f0f9e8c30e791af074c71fe07e03cdee1bec
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-10-13 19:15:49 +0600

    continuing express tutorial

--- Node Tutorial/Express/app.js
+++ Node Tutorial/Express/app.js
@@ -229,7 +229,38 @@ app.post('/login', (req, res) => {
   res.status(401).send('Please Provide Credentials')
 })
 
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
 
+  const person = people.find((person) => person.id === Number(id))
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
+
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id))
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` })
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  )
+  return res.status(200).json({ success: true, data: newPeople })
+})
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
