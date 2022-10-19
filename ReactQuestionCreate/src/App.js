dcd7239c04751ae7948156f0b6498be87c77e9f6
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-10-19 20:24:28 +0600

    initital commit of question creating feature

---
+++ ReactQuestionCreate/src/App.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import Mcq from "./components/mcq";
+import "./App.css";
+
+function App() {
+  const [serviceList, setServiceList] = useState([{ service: "" }]);
+
+  const handleServiceChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...serviceList];
+    list[index][name] = value;
+    setServiceList(list);
+  };
+
+  const handleServiceRemove = (index) => {
+    const list = [...serviceList];
+    list.splice(index, 1);
+    setServiceList(list);
+  };
+
+  const handleServiceAdd = () => {
+    setServiceList([...serviceList, { service: "" }]);
+  };
+
+  return (
+    <form className="App" autoComplete="off">
+      <div className="form-field">
+        <label htmlFor="service">Question</label>
+        {serviceList.map((singleService, index) => (
+          <div key={index} className="services">
+            <div className="first-division">
+              <Mcq
+                name="Mcq"
+                id="Mcq"
+                value={singleService.service}
+                onChange={(e) => handleServiceChange(e, index)}
+                required
+              />
+              {serviceList.length - 1 === index && (
+                <button
+                  type="button"
+                  onClick={handleServiceAdd}
+                  className="add-btn"
+                >
+                  <span>Add a Service</span>
+                </button>
+              )}
+            </div>
+            <div className="second-division">
+              {serviceList.length !== 1 && (
+                <button
+                  type="button"
+                  onClick={() => handleServiceRemove(index)}
+                  className="remove-btn"
+                >
+                  <span>Remove</span>
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="output">
+        <h2>Output</h2>
+        {serviceList &&
+          serviceList.map((singleService, index) => (
+            <ul key={index}>
+              {singleService.service && <li>{singleService.service}</li>}
+            </ul>
+          ))}
+      </div>
+    </form>
+  );
+}
+
+export default App;
