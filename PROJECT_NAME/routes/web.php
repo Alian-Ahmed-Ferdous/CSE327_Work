2a5606f23c5d92ff52c56a7a7951e8681343a8d3
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-11-03 22:24:32 +0600

    initial laravel tutorial commit

---
+++ PROJECT_NAME/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return response('<h1>Hello</h1>',200)
+    ->header('Content-Type', 'text/plain')
+    ->header('foo','bar');
+});
+
+Route::get('/users/{id}', function ($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
