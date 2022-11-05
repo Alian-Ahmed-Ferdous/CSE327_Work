94e97cdca075ceea4f2e491dbc937ebb5644c14e
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-11-05 22:00:34 +0600

    laravel tutorial complete

---
+++ PROJECT_NAME/resources/views/listings.blade.php
@@ -0,0 +1,15 @@
+{{-- <h1><?php echo $heading; ?><h1>
+<?php foreach($listings as $listing): ?>
+<h2><?php echo $listing['title']; ?></h2>
+<p><?php echo $listing['description']; ?></p>
+<?php endforeach; ?> --}}
+
+<h1>{{$heading}}</h1>
+@foreach($listings as $listing)
+<h2>
+{{$listing['title']}}
+</h2>
+<p>
+{{$listing['description']}}
+</p>
+@endforeach  
--- PROJECT_NAME/routes/web.php
+++ PROJECT_NAME/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,16 +17,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
 
-Route::get('/hello', function () {
-    return response('<h1>Hello</h1>',200)
-    ->header('Content-Type', 'text/plain')
-    ->header('foo','bar');
-});
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
 
-Route::get('/users/{id}', function ($id) {
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+// Manage Listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
