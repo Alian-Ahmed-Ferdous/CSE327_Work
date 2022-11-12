b59920fde77f0efc639958e9283c00e664a91ba8
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-11-12 20:45:06 +0600

    Making updates to the quiz creating system

--- quizSystem/src/App.js
+++ quizSystem/src/App.js
@@ -4,33 +4,113 @@ import { Grid, Button, Stack } from "@mui/material";
 import TextField from "@mui/material/TextField";
 
 export default function App() {
-  const [textQuestionList, setTextQuestionList] = useState([
-    { textQuestion: "", textAnswer: "" }
-  ]);
+  const [textQuestionList, setTextQuestionList] = useState([]);
 
-  const CreateTextQuestion = () => {};
+  const CreateTextQuestion = () => {
+    setTextQuestionList([
+      ...textQuestionList,
+      { textQuestion: "", textAnswer: "" }
+    ]);
+    console.log(textQuestionList[0]);
+  };
+
+  const handleTextQuestionChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...textQuestionList];
+    list[index][name] = value;
+    setTextQuestionList(list);
+  };
+
+  const handleTextQuestionRemove = (index) => {
+    const list = [...textQuestionList];
+    list.splice(index, 1);
+    setTextQuestionList(list);
+  };
+
+  const [audioQuestionList, setAudioQuestionList] = useState([]);
+
+  const CreateAudioQuestion = () => {
+    setAudioQuestionList([
+      ...audioQuestionList,
+      { audioQuestion: "", audioAnswer: "" }
+    ]);
+    console.log(audioQuestionList[0]);
+  };
+
+  const handleAudioQuestionChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...audioQuestionList];
+    list[index][name] = value;
+    setAudioQuestionList(list);
+  };
+
+  const handleAudioQuestionRemove = (index) => {
+    const list = [...audioQuestionList];
+    list.splice(index, 1);
+    setAudioQuestionList(list);
+  };
 
   return (
     <div>
       {textQuestionList.map((singleTextQuestion, index) => (
-        <Stack spacing={2}>
-          <Grid xs>
+        <Stack spacing={2} marginY={2} key={index}>
+          <Grid>
             <TextField
               name="textQuestion"
               id="textQuestion outlined-basic"
               label="Question"
               variant="outlined"
+              value={singleTextQuestion.textQuestion}
+              onChange={(e) => handleTextQuestionChange(e, index)}
               required
             />
           </Grid>
-          <Grid xs>
+          <Grid>
             <TextField
               name="textAnswer"
               id="textAnswer outlined-basic"
               label="Answer"
               variant="outlined"
+              value={singleTextQuestion.textAnswer}
+              onChange={(e) => handleTextQuestionChange(e, index)}
               required
             />
+          </Grid>
+          <Grid>
+            <Button className="button" onClick={handleTextQuestionRemove}>
+              Remove
+            </Button>
+          </Grid>
+        </Stack>
+      ))}
+      {audioQuestionList.map((singleAudioQuestion, index) => (
+        <Stack spacing={2} marginY={2} key={index}>
+          <Grid>
+            <TextField
+              name="audioQuestion"
+              id="audioQuestion outlined-basic"
+              label="Question"
+              variant="outlined"
+              value={singleAudioQuestion.audioQuestion}
+              onChange={(e) => handleAudioQuestionChange(e, index)}
+              required
+            />
+          </Grid>
+          <Grid>
+            <TextField
+              name="audioAnswer"
+              id="audioAnswer outlined-basic"
+              label="Answer"
+              variant="outlined"
+              value={singleAudioQuestion.audioAnswer}
+              onChange={(e) => handleAudioQuestionChange(e, index)}
+              required
+            />
+          </Grid>
+          <Grid>
+            <Button className="button" onClick={handleAudioQuestionRemove}>
+              Remove
+            </Button>
           </Grid>
         </Stack>
       ))}
@@ -45,7 +125,11 @@ export default function App() {
           </Button>
         </Grid>
         <Grid xs>
-          <Button className="button" variant="outlined">
+          <Button
+            className="button"
+            variant="outlined"
+            onClick={CreateAudioQuestion}
+          >
             Audio Question
           </Button>
         </Grid>
