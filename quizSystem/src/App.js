e35d0be807e1911f8bc6b340114e0434e36807b0
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2022-11-13 22:34:46 +0600

    updating quiz creation system

--- quizSystem/src/App.js
+++ quizSystem/src/App.js
@@ -2,18 +2,27 @@ import "./styles.css";
 import { useState } from "react";
 import { Grid, Button, Stack } from "@mui/material";
 import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function App() {
+  //To disable creating question buttons while selecting paragraph
+  const [selecting, setSelecting] = useState(true);
+
   const [textQuestionList, setTextQuestionList] = useState([]);
 
+  //Create text question(s) components
   const CreateTextQuestion = () => {
     setTextQuestionList([
       ...textQuestionList,
       { textQuestion: "", textAnswer: "" }
     ]);
-    console.log(textQuestionList[0]);
   };
 
+  //Handle input of text input fields of text question(s)
   const handleTextQuestionChange = (e, index) => {
     const { name, value } = e.target;
     const list = [...textQuestionList];
@@ -21,6 +30,13 @@ export default function App() {
     setTextQuestionList(list);
   };
 
+  //Handle submition of text question(s)
+  const handleTextQuestionSubmit = (index) => {
+    const list = [...textQuestionList];
+    console.log(list[index]);
+  };
+
+  //Handle removable of text question(s)
   const handleTextQuestionRemove = (index) => {
     const list = [...textQuestionList];
     list.splice(index, 1);
@@ -34,7 +50,6 @@ export default function App() {
       ...audioQuestionList,
       { audioQuestion: "", audioAnswer: "" }
     ]);
-    console.log(audioQuestionList[0]);
   };
 
   const handleAudioQuestionChange = (e, index) => {
@@ -44,10 +59,49 @@ export default function App() {
     setAudioQuestionList(list);
   };
 
+  const handleAudioQuestionSubmit = (index) => {
+    const list = [...audioQuestionList];
+    console.log(list[index]);
+  };
+
   const handleAudioQuestionRemove = (index) => {
     const list = [...audioQuestionList];
     list.splice(index, 1);
     setAudioQuestionList(list);
+  };
+
+  const [mcqList, setMcqList] = useState([]);
+
+  const CreateMcqQuestion = () => {
+    setMcqList([
+      ...mcqList,
+      {
+        mcqQuestion: "",
+        firstOption: "",
+        secondOption: "",
+        thirdOption: "",
+        fouthOption: "",
+        mqcAnswer: ""
+      }
+    ]);
+  };
+
+  const handleMcqQuestionChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...mcqList];
+    list[index][name] = value;
+    setMcqList(list);
+  };
+
+  const handleMcqQuestionSubmit = (index) => {
+    const list = [...mcqList];
+    console.log(list[index]);
+  };
+
+  const handleMcqQuestionRemove = (index) => {
+    const list = [...mcqList];
+    list.splice(index, 1);
+    setMcqList(list);
   };
 
   return (
@@ -80,6 +134,12 @@ export default function App() {
             <Button className="button" onClick={handleTextQuestionRemove}>
               Remove
             </Button>
+            <Button
+              className="button"
+              onClick={(e) => handleTextQuestionSubmit(index)}
+            >
+              Select
+            </Button>
           </Grid>
         </Stack>
       ))}
@@ -111,6 +171,108 @@ export default function App() {
             <Button className="button" onClick={handleAudioQuestionRemove}>
               Remove
             </Button>
+            <Button
+              className="button"
+              onClick={(e) => handleAudioQuestionSubmit(index)}
+            >
+              Select
+            </Button>
+          </Grid>
+        </Stack>
+      ))}
+      {mcqList.map((singleMcqQuestion, index) => (
+        <Stack spacing={2} marginY={2} key={index}>
+          <Grid>
+            <TextField
+              name="mcqQuestion"
+              id="mcqQuestion outlined-basic"
+              label="Question"
+              variant="outlined"
+              value={singleMcqQuestion.mcqQuestion}
+              onChange={(e) => handleMcqQuestionChange(e, index)}
+              required
+            />
+          </Grid>
+          <Grid container>
+            <Grid item xs={3}>
+              <Grid marginRight={1} paddingBottom={1}>
+                <TextField
+                  name="firstOption"
+                  id="firstOption outlined-basic"
+                  label="A"
+                  variant="outlined"
+                  value={singleMcqQuestion.firstOption}
+                  onChange={(e) => handleMcqQuestionChange(e, index)}
+                  required
+                />
+              </Grid>
+              <Grid marginRight={1} paddingTop={1}>
+                <TextField
+                  name="thirdOption"
+                  id="thirdOption outlined-basic"
+                  label="C"
+                  variant="outlined"
+                  value={singleMcqQuestion.thirdOption}
+                  onChange={(e) => handleMcqQuestionChange(e, index)}
+                  required
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={3}>
+              <Grid marginLeft={1} paddingBottom={1}>
+                <TextField
+                  name="secondOption"
+                  id="secondOption outlined-basic"
+                  label="B"
+                  variant="outlined"
+                  value={singleMcqQuestion.secondOption}
+                  onChange={(e) => handleMcqQuestionChange(e, index)}
+                  required
+                />
+              </Grid>
+              <Grid marginLeft={1} paddingTop={1}>
+                <TextField
+                  name="fouthOption"
+                  id="fouthOption outlined-basic"
+                  label="D"
+                  variant="outlined"
+                  value={singleMcqQuestion.fouthOption}
+                  onChange={(e) => handleMcqQuestionChange(e, index)}
+                  required
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <FormControl>
+              <InputLabel name="mqcAnswer" id="mqcAnswer">
+                Answer
+              </InputLabel>
+              <Select
+                name="mqcAnswer"
+                id="mqcAnswer"
+                label="mqcAnswer"
+                value={singleMcqQuestion.mqcAnswer}
+                onChange={(e) => handleMcqQuestionChange(e, index)}
+              >
+                <MenuItem value={"A"}>A</MenuItem>
+                <MenuItem value={"B"}>B</MenuItem>
+                <MenuItem value={"C"}>C</MenuItem>
+                <MenuItem value={"D"}>D</MenuItem>
+              </Select>
+              <FormHelperText>Select the correct option</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid>
+            <Button className="button" onClick={handleMcqQuestionRemove}>
+              Remove
+            </Button>
+            <Button
+              className="button"
+              onClick={() => handleMcqQuestionSubmit(index)}
+            >
+              Select
+            </Button>
           </Grid>
         </Stack>
       ))}
@@ -134,7 +296,11 @@ export default function App() {
           </Button>
         </Grid>
         <Grid xs>
-          <Button className="button" variant="outlined">
+          <Button
+            className="button"
+            variant="outlined"
+            onClick={CreateMcqQuestion}
+          >
             MCQ
           </Button>
         </Grid>
